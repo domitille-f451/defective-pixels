775d483b5f4b17a48f0527fbd8690d5ee90d70bc
Domitille Debret <30366285+domitille-f451@users.noreply.github.com>
2019-11-23 14:44:12 +0100

    Add Files

---
+++ defectivepixels.js
@@ -0,0 +1,30 @@
+var numberOfDiv = 2000; // Define number of Defective Pixels
+
+for (var i = 0; i < numberOfDiv; i++) {
+  var newDiv = document.createElement("div");
+  var states = ["hot"]; // define the type of defective pixels "hot" "dead" "stuck"
+  var colors = ["rgb(255,0,0)", "rgb(0,255,0)", "rgb(0,0,255)"];
+  var windowH = window.innerHeight;
+  var windowW = window.innerWidth;
+
+  var data = {
+    top: chance.integer({ min: 1, max: windowH }),
+    left: chance.integer({ min: 1, max: windowW }),
+    state: states[Math.floor(Math.random() * states.length)],
+    color: colors[Math.floor(Math.random() * colors.length)]
+  };
+
+  if (localStorage.getItem("pixel" + i) == null) {
+    localStorage.setItem("pixel" + i, JSON.stringify(data));
+    var storedData = data;
+  } else {
+    var storedData = JSON.parse(localStorage.getItem("pixel" + i));
+  }
+
+  newDiv.classList = "pixel " + storedData.state;
+  newDiv.style.top = storedData.top;
+  newDiv.style.left = storedData.left;
+  if (storedData.state == "stuck") newDiv.style.background = storedData.color;
+
+  document.body.appendChild(newDiv);
+}
---
+++ defectivepixelscookie.js
@@ -0,0 +1,43 @@
+var numberOfDiv = 10; // Define number of Defective Pixels
+var currentLength = Cookies.get("pixelLength");
+var states = ["hot", "stuck"];
+var colors = ["rgb(255,0,0)", "rgb(0,255,0)", "rgb(0,0,255)"];
+var windowH = window.innerHeight;
+var windowW = window.innerWidth;
+
+var data = !Cookies.get("data") ? [] : eval(JSON.parse(Cookies.get("data")));
+
+if (data.length > 0) {
+  for (var i = 0; i < currentLength; i++) {
+    console.log(data[i]);
+    var pixelData = data[i];
+    placePixel(pixelData);
+  }
+}
+
+for (var i = 0; i < numberOfDiv; i++) {
+  var pixel = !currentLength ? i : currentLength * 1 + i;
+  var pixelData = [
+    chance.integer({ min: 1, max: windowH }),
+    chance.integer({ min: 1, max: windowW }),
+    states[Math.floor(Math.random() * states.length)],
+    colors[Math.floor(Math.random() * colors.length)]
+  ];
+  data[pixel] = pixelData;
+  placePixel(pixelData);
+}
+
+var newLength = !currentLength ? 10 : currentLength * 1 + 10;
+Cookies.set("pixelLength", newLength);
+Cookies.set("data", JSON.stringify(data));
+
+function placePixel(storedData) {
+  if (storedData) {
+    var newDiv = document.createElement("div");
+    newDiv.classList = "pixel " + storedData[2];
+    newDiv.style.top = storedData[0];
+    newDiv.style.left = storedData[1];
+    if (storedData[2] == "stuck") newDiv.style.background = storedData[3];
+    document.body.appendChild(newDiv);
+  }
+}
